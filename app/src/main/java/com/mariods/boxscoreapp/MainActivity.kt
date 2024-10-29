5770bec11001a3ff025f3a2a042cd62120be12fb
Mario DS <mariostenner@gmail.com>
2024-10-29 10:04:45 -0600

    Revert "Revert "Se agregaron las librerias de firebase para utilizar crashlytics, autenticacion y firestore""
    This reverts commit 45a6de49d7935790e594cdc405ce9322db303224.

--- app/src/main/java/com/mariods/boxscoreapp/MainActivity.kt
+++ app/src/main/java/com/mariods/boxscoreapp/MainActivity.kt
@@ -19,6 +19,8 @@ class MainActivity : ComponentActivity() {
             BoxScoreAppTheme {
                 //val mainViewModel: MainViewModel = hiltViewModel()
                 NavigationWrapper()
+
+
                 //MainScreen()
             }
         }
